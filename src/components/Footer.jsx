ed6db1c801b6a52f7c608f5df7a5a4d6317daead
ejd500 <138682375+ejd500@users.noreply.github.com>
2023-12-20 11:48:15 -0330

    Scroll to top of contact page added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,14 +11,14 @@ const Footer = () => {
 
         <div id="phone-email">
             <h2>Let's Connect!</h2>
-            <p><Link to="/contact"><FontAwesomeIcon icon={faEnvelope} size="1x"/>&nbsp;ellendalton045@gmail.com</Link></p>
+            <p><Link to="/contact" onClick={()=>{window.scrollTo({top: 0})}}><FontAwesomeIcon icon={faEnvelope} size="1x"/>&nbsp;ellendalton045@gmail.com</Link></p>
             <p><Link to="/contact"><FontAwesomeIcon icon={faPhone} size="1x"/>&nbsp;709-746-2252</Link></p>
         </div>
         <div id="social-media">
             <div className="icons">
                 <a href="https://www.linkedin.com/in/ellen-dalton-a4236a70" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
                 <a href="https://github.com/ejd500" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-                <Link to="/contact"><FontAwesomeIcon icon={faEnvelope} size="2x"/></Link>
+                <Link to="/contact"><FontAwesomeIcon icon={faEnvelope} size="2x" onClick={()=>{window.scrollTo({top: 0})}}/></Link>
             </div>
             <div id="copyright">
                 &copy; EJD 2023
